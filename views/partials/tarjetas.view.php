84c7384649f23fdca328838310ca483553884eb3
Fernando Peralta Bustamante <fernandoperaltab4@gmail.com>
2023-07-11 23:10:10 -0500

    Se corrigieron los css y se agrego tarjetas a partials

---
+++ views/partials/tarjetas.view.php
@@ -0,0 +1,16 @@
+<div class="col">
+          <div class="card h-100">
+            <div id="profe-6">
+              <div class="image_profesores">
+                <img src="media/Profesores/jose_ochoa.png">
+              </div>
+              <div class="title">
+                <h1>Dr. Jose Eduardo Ochoa Luna</h1>
+              </div>
+              <div class="des">
+                <p>Línea de investigación: Inteligencia Artificial.</p>
+              </div>
+            </div>
+            <button profe_id="6" onclick="showProfe(this)" class="button_ver_mas"> Ver más </button>
+          </div>
+</div>
