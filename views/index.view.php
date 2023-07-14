df3366157c1d0c6c7bfaa47d5980dc7fc31420e4
Fernando Peralta Bustamante <fernandoperaltab4@gmail.com>
2023-07-14 00:45:34 -0500

    Carrusel con estilos

--- views/index.view.php
+++ views/index.view.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="styles/footer.css">
     <link rel="stylesheet" href="styles/proyeccion.css">
     <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/carousel.css">
     </head>
     <!--En el Carousel el objetivo es tambien que este se pueda extraer mediante templates debido a el codigo repetitivo que presenta-->
     <!--cualquier cosa-->
