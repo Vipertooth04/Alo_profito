43eaf175b0bfdca3cea139fcd19a59c58851dd13
nxms806 <NXimenasilva@outlook.com>
2023-07-14 00:39:30 -0500

    img y nv

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -2,13 +2,13 @@
     <div class="navbar">
         <img id="logo" src="media/iconos/logo-departamento.png" >
         <div class="opciones">
-            <a href="#">Inicio</a>
-            <a href="#">Acerca de</a>
-            <a href="#">Pregrado</a>
-            <a href="#">Postgrado</a>
-            <a href="#">Investigación e innovación (RICS)</a>
-            <a href="#">Proyección social</a>
-            <a href="#">STAFF</a> 
+            <a href="http://localhost/Alo_profito/">Inicio</a>
+            <a>Acerca de</a>
+            <a>Pregrado</a>
+            <a>Postgrado</a>
+            <a>Investigación e innovación (RICS)</a>
+            <a>Proyección social</a>
+            <a>STAFF</a> 
         </div>
     </div>
 </body>
