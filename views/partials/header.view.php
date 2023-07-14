54b103ded265c15364642efbb76e7b435026f478
nxms806 <NXimenasilva@outlook.com>
2023-07-13 23:11:00 -0500

    Se agrego el navbar y se modifico la flecha

---
+++ views/partials/header.view.php
@@ -0,0 +1,14 @@
+<body>
+    <div class="navbar">
+        <img id="logo" src="media/iconos/logo-departamento.png" >
+        <div class="opciones">
+            <a href="#">Inicio</a>
+            <a href="#">Acerca de</a>
+            <a href="#">Pregrado</a>
+            <a href="#">Postgrado</a>
+            <a href="#">Investigación e innovación (RICS)</a>
+            <a href="#">Proyección social</a>
+            <a href="#">STAFF</a> 
+        </div>
+    </div>
+</body>
