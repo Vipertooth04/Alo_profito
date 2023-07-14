4f92c3ca76271de2022c1b530509567c6956cf2b
Ailing12s1 <ailinangelica2020@gmail.com>
2023-07-13 22:50:12 -0500

    Holi aca va el mensaje

--- views/partials/footer.view.php
+++ views/partials/footer.view.php
@@ -7,7 +7,7 @@
             <p>Universidad Católica San Pablo</p>
             <p>Campus San Lázaro - Quinta Vivanco s/n Urb. Campiña Paisajista, Arequipa</p>
             <p>+51 54 605630 | +51 54 605600 </p>
-            <p><a href="institucional@ucsp.edu.pe" class="email">institucional@example.com</a></p>
+            <p><a href="mailto:institucional@ucsp.edu.pe" class="email-link">institucional@ucsp.edu.pe</a></p>
             <div class="imagen-container">
                 <a href="https://web.facebook.com/ucatolicasanpablo?_rdc=1&_rdr" target="_blank">
                     <img src="media/iconos/facebook.png"></a>
@@ -22,13 +22,13 @@
             </div>    
         </div>
         <div class="column">
-            <p><a href="https://www.google.com/" class="link">TRABAJA CON NOSOTROS</a>
-            <p><a href="https://www.google.com/" class="link">POLÍTICA DE PRIVACIDAD</a>
-            <p><a href="https://www.google.com/" class="link">LIBRO DE RECLAMACIONES</a>
-            <p><a href="https://www.google.com/" class="link">BUZÓN DE SUGERENCIAS</a>
-            <p><a href="https://www.google.com/" class="link">TRÁMITES UNIVERSITARIOS</a>
-            <p><a href="https://www.google.com/" class="link">DEFENSORÍA UNIVERSITARIA</a>
-            <p><a href="https://www.google.com/" class="link">DERECHOS ARCO</a>
+            <p><a href="https://cs.ucsp.edu.pe/?_ga=2.40720226.1554117585.1689109213-541285207.1675818269#" class="link">TRABAJA CON NOSOTROS</a>
+            <p><a href="https://ucsp.edu.pe/politica-de-privacidad/" class="link">POLÍTICA DE PRIVACIDAD</a>
+            <p><a href="https://ucsp.edu.pe/libro-de-reclamaciones/" class="link">LIBRO DE RECLAMACIONES</a>
+            <p><a href="https://sugerencias.ucsp.edu.pe/" class="link">BUZÓN DE SUGERENCIAS</a>
+            <p><a href="https://ucsp.edu.pe/tramites" class="link">TRÁMITES UNIVERSITARIOS</a>
+            <p><a href="https://ucsp.edu.pe/defensoria-universitaria" class="link">DEFENSORÍA UNIVERSITARIA</a>
+            <p><a href="https://ucsp.edu.pe/derechos-arco" class="link">DERECHOS ARCO</a>
         </div>
       </div>   
     </footer>
