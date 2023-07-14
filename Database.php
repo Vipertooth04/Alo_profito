ab5dc6ba2894247bc1b492fa32602f58a1d25144
Fernando Peralta Bustamante <fernandoperaltab4@gmail.com>
2023-07-13 22:47:02 -0500

    encapsulados prueba 1

--- Database.php
+++ Database.php
@@ -6,20 +6,62 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", "root");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Ejemplo de consulta para obtener datos de una tabla llamada 'usuarios'
+    // profesores
     $stmt = $conn->query("SELECT nombre, descripcion FROM profesores_cs");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Generar el código HTML con los resultados de la consulta
-    echo "<table>";
-    foreach ($results as $row) {
-        echo "<tr>";
-        echo "<td>" . $row['nombre'] . "</td>";
-        echo "<td>" . $row['descripcion'] . "</td>";
-        // Agrega más columnas según la estructura de tu tabla
-        echo "</tr>";
+    // Especialidad
+    $stmt2 = $conn->query("SELECT p.nombre AS profesor, e.especialidad AS especialidades
+    FROM profesores_cs p
+    JOIN profesores_especialidades pe ON p.id_profesor = pe.id_profesor
+    JOIN especialidades e ON pe.id_especialidad = e.id_especialidades
+    LIMIT 0, 25;
+    ");
+    $especialidad = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //titulo
+    $stmt3 = $conn->query("SELECT DISTINCT p.nombre AS profesor,t.titulo AS titulo, t.institucion AS institucion,t.pais AS pais,pt.anio AS year
+    FROM profesores_cs p
+    JOIN profesores_titulos pt ON p.id_profesor = pt.id_profesor
+    JOIN titulos_academicos t ON pt.id_titulo = t.id_titulo
+    LIMIT 0, 25;");
+    $titulos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    
+    // correo-electronico
+    $stmt4 = $conn->query("SELECT p.nombre AS profesor, c.correo_electronico AS correo
+    FROM profesores_cs p
+    JOIN correos_electronicos c WHERE p.id_profesor=c.id_profesor;");
+    $correo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $row){
+        $profesor=array(
+            'nombre'=>$row['nombre'],
+            'descripcion'=>$row['descripcion'],
+            'especialidades' => array(),
+            'titulos' => array(),
+            'correos_electronicos'=>array()
+        );
+
+        foreach ($especialidad as $esp){
+            if ($esp['profesor']===$nombre){
+                $profesor['especialidades'][]=$esp['especialidades'];
+            }
+        }
+
+        foreach ($titulos as $til){
+            if ($til['profesor']===$nombre){
+                $profesor['titulos'][]=$til['titulo'];
+            }
+        }
+
+        foreach ($correo as $cor){
+            if ($cor['profesor']===$nombre){
+                $profesor['correos_electronicos'][]=$til['correo'];
+            }
+        }
     }
-    echo "</table";
+    
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
