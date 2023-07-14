d797ca2806b4f1be5bbdbc066ca49bbcbd9ed188
nxms806 <NXimenasilva@outlook.com>
2023-07-14 00:52:33 -0500

    nv

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -3,12 +3,11 @@
         <img id="logo" src="media/iconos/logo-departamento.png" >
         <div class="opciones">
             <a href="http://localhost/Alo_profito/">Inicio</a>
-            <a>Acerca de</a>
-            <a>Pregrado</a>
-            <a>Postgrado</a>
-            <a>Investigación e innovación (RICS)</a>
-            <a>Proyección social</a>
-            <a>STAFF</a> 
+            <a href="https://cs.ucsp.edu.pe/acerca-de/">Acerca de</a>
+            <a href="https://cs.ucsp.edu.pe/pregrado/">Pregrado</a>
+            <a href="https://cs.ucsp.edu.pe/postgrado/">Postgrado</a>
+            <a href="https://cs.ucsp.edu.pe/investigacion/">Investigación e innovación (RICS)</a>
+            <a href="https://cs.ucsp.edu.pe/proyeccion-social/">Proyección social</a>
         </div>
     </div>
 </body>
